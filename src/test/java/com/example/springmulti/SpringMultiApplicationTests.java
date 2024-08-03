bf4a604013deba1b91ff08006b458021a54da3a5
WooJJam <111514410+WooJJam@users.noreply.github.com>
2024-08-04 02:58:15 +0900

    init: 초기 프로젝트 생성

---
+++ src/test/java/com/example/springmulti/SpringMultiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmulti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
