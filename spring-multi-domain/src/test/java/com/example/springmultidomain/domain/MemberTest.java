48bbbb7b7de42021e78cb4899b6f9818b2162a28
WooJJam <111514410+WooJJam@users.noreply.github.com>
2024-08-20 20:52:59 +0900

    test: Member 테스트 주석처리

--- spring-multi-domain/src/test/java/com/example/springmultidomain/domain/MemberTest.java
+++ spring-multi-domain/src/test/java/com/example/springmultidomain/domain/MemberTest.java
@@ -1,28 +1,28 @@
-package com.example.springmultidomain.domain;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.example.springmultidomain.MemberRepository;
-
-@SpringBootTest
-public class MemberTest {
-
-	@Autowired
-	MemberRepository memberRepository;
-
-	@Test
-	public void 멤버_테스트() throws Exception {
-
-		Member member = Member.builder()
-			.name("jaemin")
-			.password("1234")
-			.build();
-
-		Member findMember = memberRepository.save(member);
-		Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
-	}
-}
+// package com.example.springmultidomain.domain;
+//
+// import org.assertj.core.api.Assertions;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.SpringBootConfiguration;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// import com.example.springmultidomain.MemberRepository;
+//
+// @SpringBootTest
+// public class MemberTest {
+//
+// 	@Autowired
+// 	MemberRepository memberRepository;
+//
+// 	@Test
+// 	public void 멤버_테스트() throws Exception {
+//
+// 		Member member = Member.builder()
+// 			.name("jaemin")
+// 			.password("1234")
+// 			.build();
+//
+// 		Member findMember = memberRepository.save(member);
+// 		Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+// 	}
+// }
