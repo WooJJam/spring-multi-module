36269a6343b93606b3f3b8c49430519dd15c7acf
WooJJam <111514410+WooJJam@users.noreply.github.com>
2024-09-01 23:48:40 +0900

    chore: CI 빌드문 수정

--- spring-multi-domain/src/test/java/com/example/springmultidomain/RepositoryLocation.java
+++ spring-multi-domain/src/test/java/com/example/springmultidomain/RepositoryLocation.java
@@ -1,8 +1,6 @@
 package com.example.springmultidomain;
 
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class RepositoryLocation {
